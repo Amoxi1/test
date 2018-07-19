12b727ae024376108a70e0a92ca0346cc71a3429
test <760559321@qq.com>
2018-07-19 15:07:37 +0800

    功能差不多都有了

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
-window.onload = function(){
-    var tabnav = document.querySelector(".nav");
+   var tabnav = document.querySelector(".nav");
     var alltitle = tabnav.querySelectorAll("li");
     var content = document.querySelector(".list");
     var allConent = content.querySelectorAll("ul");
@@ -17,17 +16,17 @@ window.onload = function(){
 
     }
 
-    var todoIpt = document.getElementById("todoValue");
-    var send = document.getElementById("send");
-    var clear = document.getElementById("clear");
+    var todoIpt = document.querySelector("#todoValue");
+    var send = document.querySelector("#send");
+    var clear = document.querySelector("#clear");
     var todoListArray = [];
+    var obj_list = {
+        todo:"",  //储存输入值
+        done:false   //判断事项状态
+    }
     createList();
     //储存用户输入值
     function saveValue(){
-        var obj_list = {
-            todo:"",  //储存输入值
-            done:false   //判断事项状态
-        }
         var todoValues = todoIpt.value ;
         if(!todoValues){
             alert("输入内容不可为空");
@@ -55,14 +54,15 @@ window.onload = function(){
         if(todoListArray != null){
             for(var i=0;i<todoListArray.length;i++){
                 if(!todoListArray[i].done){//未完成
-                    todoHtml +="<li><span>内容" + todoListArray[i].todo + "</span><input type='text' /><b>修改</b><i>删除</i><p class='finished' onclick=''>完成</p></li>"
+                    todoHtml +="<li><span>内容" + todoListArray[i].todo + "</span><b onclick='edit("+i+")'>修改</b><i class='delete' onclick='remove("+i+")'>删除</i><p class='status' onclick='changeStatus("+i+", \"done\", true)'>待完成</p></li>"
                 }else{
-                    doneHtml +="<li><span>内容" + todoListArray[i].todo + "</span><input type='text' /><b>修改</b><i>删除</i></li>"
+                    doneHtml +="<li><span>内容" + todoListArray[i].todo + "</span><b>修改</b><i class='delete' onclick='remove("+i+")'>删除</i><p class='status'>已完成</p></li>"
                 }
             }
             todolist.innerHTML = todoHtml
             donelist.innerHTML = doneHtml
             allList.innerHTML = todoHtml + doneHtml
+
         }else{
             allList.innerHTML = "";
             todolist.innerHTML =  "";
@@ -82,27 +82,46 @@ window.onload = function(){
             return [];
         }
     }
-    //点击完成修改状态
-    var statusChange = document.querySelector
-    
-
     //添加待办事项
-    send.onclick = function(){
+    send.addEventListener("click",function(){
         saveValue();
-    }
+    },false)
 
     //回车添加事项
-    todoIpt.onkeydown = function(e){
+    todoIpt.addEventListener("keydown",function(e){
         var e = e ||window.event;
         if(e.keyCode == 13){
             saveValue();      
         }
+    },false)
 
+    //删除单个事项
+    function remove(i) {
+        todoListArray.splice(i, 1);
+        saveData(todoListArray);
+        createList();
     }
-
+    //修改事项状态
+    function changeStatus(i, done, value) {  
+        todoListArray[i][done] = value; 
+        console.log(todoListArray)
+        console.log("i",i)
+        console.log('done',done)
+        console.log('value',value)
+        saveData(todoListArray);  
+        createList();
+    }
+    //编辑内容
+    function edit(i){
+        if(!todoIpt.value){
+            alert("修改内容不可为空")
+        }else{
+            changeStatus(i,"todo",todoIpt.value)
+        }
+    }
+    
     //删除全部
-    clear.onclick = function(){
+    clear.addEventListener("click",function(){
         localStorage.clear()
         location.reload();
-    }
-}
+    },false)
